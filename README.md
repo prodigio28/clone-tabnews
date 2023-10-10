9ab94f539faaff5de93255f24c55eb8f6b876b18
Gusta <145910956+gugal28@users.noreply.github.com>
2023-10-10 20:24:22 +0000

    Adiciona script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para começar o curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>E meu açai nada ainda??</h1>
+  return <h1>E meu açai nada ainda??</h1>;
+}
+
+function teste() {
+  console.log("arroz doce");
 }
 
 export default Home;
